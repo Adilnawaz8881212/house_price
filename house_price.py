0880954d6e5bd913156a787711061e5964376995
Adil nawaz <134604273+Adilnawaz8881212@users.noreply.github.com>
2024-07-12 21:41:16 -0700

    Add files via upload

---
+++ house_price.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from xgboost import XGBRegressor
+
+# Load the model
+with open('xgboost_model.pkl', 'rb') as model_file:
+    pipeline = pickle.load(model_file)
+
+# Load the DataFrame
+with open('df.pkl', 'rb') as df_file:
+    df = pickle.load(df_file)
+
+# Title of the app
+st.title("Real Estate Price Prediction")
+
+# Sidebar for user input
+a = st.selectbox('Location Name', df['location'].unique())
+b = st.number_input("Bedrooms", 0, 100)
+c = st.number_input("Bath", 0, 10)
+d = st.number_input("Size", 0, 100000)
+e = st.selectbox('West open', df['west_open'].unique())
+f = st.selectbox('Attach bath', df['Attach_bath'].unique())
+
+if st.button("Predict price"):
+    # Prepare input data
+    user_input = pd.DataFrame([[a,b,c,d,e,f]], columns=['location', 'bedrooms', 'baths', 'size','west_open','Attach_bath'])
+
+    # Make Prediction
+    predicted_price = int(np.exp(pipeline.predict(user_input)[0]))
+    
+
+    # Display the Prediction with a meaningful message
+    st.title(f'Your Home Price is: {predicted_price} PKR')
+
+
